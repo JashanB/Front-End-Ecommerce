22319f42249d9e4475de2b2e2a12af442332c16b
JashanB <jashan_brar50@hotmail.com>
2020-09-07 16:53:47 -0400

    imported fakestore data

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const axios = require('axios');
 
 
 function App() {
-  const 
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    axios.get('https://fakestoreapi.com/products')
+    .then(res => {
+      let result = res.data
+      setProducts(state => ([...products, result]))
+      // console.log("PRODUCTS", res.data)
+      for (let product of res.data) {
+        console.log(product)
+        if (categories.indexOf(products.category == -1)) {
+          setCategories([...categories, product.category]);
+        }
+      }
+      })
+  }, [])
+  console.log("products", products)
+  console.log(categories)
   return (
     <div className="App">
       <header className="App-header">
