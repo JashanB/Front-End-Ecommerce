1fbd5a9fc3df792c8047e6e786d1487347bd8dde
JashanB <jashan_brar50@hotmail.com>
2020-09-07 16:01:54 -0400

    setup file structure

---
+++ src/components/app/App.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import './App.css';
+
+const axios = require('axios');
+
+
+function App() {
+  const 
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          React app woo
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
